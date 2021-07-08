087c3c6b70319c13f63c4951d38d1a7b3107a84a
cadgerfeast <cadgerfeast@users.noreply.github.com>
2021-07-08 10:19:55 +0000

    deploy: baa4134455b925393a4d63983004986b7a33dee5

---
+++ _nuxt/static/1625739522/playground/amd/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:".\u002F_nuxt\u002Fstatic\u002F1625739522",layout:"default",error:a,serverRendered:true,routePath:"\u002Fplayground\u002Famd",config:{_app:{basePath:"\u002Fmodkit-loader\u002F",assetsPath:".\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f673ebbe"}}}}(null));
