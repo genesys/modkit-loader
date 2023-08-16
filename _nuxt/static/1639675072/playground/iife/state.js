d496bc20594db10105d8746b6d9b9adca46e99a6
tichon29 <tichon29@users.noreply.github.com>
2023-08-16 09:58:05 +0000

    deploy: fe1c4e4f66df653bfc8b0a0cfacd76418c3b4242

--- _nuxt/static/1639675072/playground/iife/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:".\u002F_nuxt\u002Fstatic\u002F1639675072",layout:"default",error:a,serverRendered:true,routePath:"\u002Fplayground\u002Fiife",config:{_app:{basePath:"\u002Fmodkit-loader\u002F",assetsPath:".\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cec94df2"}}}}(null));
