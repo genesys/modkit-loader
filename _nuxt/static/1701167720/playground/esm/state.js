69062b2590a1b73e7ae06bea3ef39a635c061db9
tichon29 <tichon29@users.noreply.github.com>
2023-11-28 10:36:12 +0000

    deploy: 82eb7afca893bf996140df5b0da218212b51d9ee

---
+++ _nuxt/static/1701167720/playground/esm/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:".\u002F_nuxt\u002Fstatic\u002F1701167720",layout:"default",error:a,serverRendered:true,routePath:"\u002Fplayground\u002Fesm",config:{_app:{basePath:"\u002Fmodkit-loader\u002F",assetsPath:".\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ed4f34ec"}}}}(null));
