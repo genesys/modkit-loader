7c2ecf25653cc816a726362ccef6f01f462aecf8
tichon29 <tichon29@users.noreply.github.com>
2024-09-10 08:56:30 +0000

    deploy: 141f0dd36dc7067b6f4a5f6acc2bd896969049a7

--- _nuxt/static/1701167720/playground/umd/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:".\u002F_nuxt\u002Fstatic\u002F1701167720",layout:"default",error:a,serverRendered:true,routePath:"\u002Fplayground\u002Fumd",config:{_app:{basePath:"\u002Fmodkit-loader\u002F",assetsPath:".\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ed4f34ec"}}}}(null));
